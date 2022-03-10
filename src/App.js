a0469300f9dda96dc0b05d4491a22694879ebd27
TumiPoise <tumininu.sosanya@gmail.com>
2022-03-09 21:57:44 -0800

    Created the footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/pages/Home.js";
+import Services from "./components/pages/Services";
+import SignUp from "./components/pages/SignUp";
+import Products from "./components/pages/Products";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -10,9 +14,12 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" exact />
+          <Route path="/" exact element={<Home />} />
+          <Route path="/services" exact element={<Services />} />
+          <Route path="/products" exact element={<Products />} />
+          <Route path="/sign-up" exact element={<SignUp />} />
         </Routes>
-        <Home />
+        <Footer />
       </Router>
     </>
   );
